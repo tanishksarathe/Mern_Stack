a5746f796ca29c7554a7c37cd4a594e20ef76eb0
tanishksarathe <tanishksarathe19112@gmail.com>
2025-12-13 11:08:12 +0530

    Validation Done

--- Registration Form Coaching/script.js
+++ Registration Form Coaching/script.js
@@ -8,6 +8,8 @@ function submit() {
       selectedBatch.push(element.value);
     });
 
+    document.querySelectorAll(".Error").innerText = "";
+
 
     const fullname = document.getElementById("fullname").value;
     const email = document.getElementById("email").value;
@@ -38,6 +40,88 @@ function submit() {
     console.log(guardian);
     console.log(hear);
     console.log(special);
+
+    if(!/^[A-Za-z ]+$/.test(fullname)){
+      document.getElementById("namevali").innerText = "Please enter a valid name";
+      return;
+    }
+
+    if(!/^[A-Za-z ]+$/.test(city)){
+      document.getElementById("cityvali").innerText = "Please enter a valid city name";
+      return;
+    }
+
+    if(!/^\d{6}$/.test(pin)){
+      document.getElementById("pinvali").innerText = "Please enter a valid 6 digit pin code";
+      return;
+    }
+
+
+
+    if(!/^[A-Z]-[\w ,]+$/.test(address)){
+      document.getElementById("addvali").innerText = "Please enter your full address";
+      return;
+    }
+
+    if(!/^[A-Za-z ]+$/.test(guardian)){
+      document.getElementById("gnamevali").innerText = "Please full name of guardian";
+      return;
+    }
+
+    if(!/^[\w\.]+@(gmail|ricr|outlook)\.(com|in|org)$/.test(email)){
+      document.getElementById("emailvali").innerText = "Please enter a valid email address";
+      return;
+    }
+
+    if(!/^[6-9]\d{9}$/.test(mobileno)){
+      document.getElementById("phonevali").innerText = "Please enter a valid phone number";
+      return;
+    }
+
+    if(!/^[6-9]\d{9}$/.test(guardianContact)){
+      document.getElementById("gphonevali").innerText = "Please enter a valid (10 digit) phone number of your guardian";
+      return;
+    }
+
+    //Age Verification
+
+    const today = new Date().getFullYear();
+
+    if((today - birth.getFullYear()) < 15){
+      document.getElementById("dobvali").innerText = "You must be atleast 15 years old";
+      return;
+    }
+
+
+    if(qualification === ""){
+       document.getElementById("qualvali").innerText = "Please select a qualification";
+      return;
+    }
+
+    if(hear === ""){
+       document.getElementById("hearvali").innerText = "Please select an Option";
+      return;
+    }
+
+    if(course === ""){
+       document.getElementById("coursevali").innerText = "Please select a course";
+      return;
+    }
+
+    if(selectedBatch == [null]){
+      documnet.getElementById("daysvali").innerText = "Please select a batch timing";
+      return;
+    }
+
+
+
+
+
+
+    
+
+
+
 
 
     console.log(selectedBatch);
